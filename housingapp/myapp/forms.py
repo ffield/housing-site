5f5a82c6042085a5d4badd56be4c0e7d44492170
ffield <ffield@udel.edu>
2017-02-03 11:22:01 -0500

    Add files via upload

--- housingapp/myapp/forms.py
+++ housingapp/myapp/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 
+
+CATEGORIES = (  
+    ('People', 'people'),
+    ('Rooms', 'rooms'),
+    ('Price', 'price'),
+)
+
+
 class ReviewForm(forms.Form):
-	reviewTitle = forms.CharField(label='Title')
-	review = forms.CharField(label='Review Here')
-	propertyRating = forms.FloatField()
-	landlordRating = forms.FloatField()
+	reviewTitle = forms.CharField(label='Title',widget=forms.TextInput(attrs={'class': "titleField"}))
+	review = forms.CharField(label='Review Here', widget=forms.Textarea(attrs={'class': "reviewfield"}))
+	propertyRating = forms.FloatField(min_value=0.0, max_value=5.0,
+		label = 'Property Rating',widget=forms.TextInput(attrs={'class': "ratingField"}))
+	landlordRating = forms.FloatField(min_value=0.0, max_value=5.0,
+		label = 'Landlord Rating',widget=forms.TextInput(attrs={'class': "ratingField"}))
 
 class LoginForm(forms.Form):
 	username = forms.CharField(label = 'email')
@@ -13,4 +23,7 @@ class LoginForm(forms.Form):
 class RegisterForm(forms.Form):
 	username = forms.CharField(label = 'email')
 	password = forms.CharField(label = 'password', widget=forms.PasswordInput())
+
+class SortForm(forms.Form):
+	sortBy = forms.ChoiceField(label = 'Sort', choices=CATEGORIES )
 
